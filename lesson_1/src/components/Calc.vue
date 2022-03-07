3c72202cd7964801f6fda0deef1ef5c2cb9c6642
Vladimir Vladimir <peshkovdev@gmail.com>
2022-03-07 16:01:39 +0300

    ДЗ_2

--- lesson_1/src/components/Calc.vue
+++ lesson_1/src/components/Calc.vue
@@ -1,18 +1,59 @@
 <template>
-    <form class="calc" action="" @click = "getResult($event)">
-        <input class="calc__operand1" type="number" v-model.number.trim="operand1">
-        <input class="calc__operand2" type="number" v-model.number.trim="operand2">
+    <form class="calc">
+        <input class="calc__operand1" type="number" v-model="operand1">
+        <input class="calc__operand2" type="number" v-model="operand2">
         <span class="calc__result"> = {{result}}</span>
         <ul class="calc__buttons">
-            <li class="calc__btn"><input class="calc__btn-input calc__btn-input--sum" type="submit" value="+"></li>
-            <li class="calc__btn"><input class="calc__btn-input calc__btn-input--diff" type="submit" value="-"></li>
-            <li class="calc__btn"><input class="calc__btn-input calc__btn-input--mult" type="submit" value="*"></li>
-            <li class="calc__btn"><input class="calc__btn-input calc__btn-input--div" type="submit" value="/"></li>
-            <li class="calc__btn"><input class="calc__btn-input calc__btn-input--div--int" type="submit" value="Целочисленное деление"></li>
+
+            <li class="calc__btn" @click="sum">
+                <input 
+                    class="calc__btn-input calc__btn-input--sum" 
+                    type="submit" 
+                    value="+"
+                />
+            </li>
+            
+            <li class="calc__btn" @click="diff">
+                <input 
+                    class="calc__btn-input calc__btn-input--diff" 
+                    type="submit" 
+                    value="-"
+                />
+            </li>
+
+            <li class="calc__btn" @click="mult">
+                <input 
+                    class="calc__btn-input calc__btn-input--mult" 
+                    type="submit" 
+                    value="*"
+                />
+            </li>
+
+            <li class="calc__btn" @click="div">
+                <input 
+                    class="calc__btn-input calc__btn-input--div" 
+                    type="submit" 
+                    value="/"
+                />
+            </li>
+
+            <li class="calc__btn" @click = "getResult($event)">
+                <input 
+                    class="calc__btn-input calc__btn-input--div--int" 
+                    type="submit" 
+                    value="Целочисленное деление"
+                />
+            </li>
         </ul>
-        <label for="calc__checkbox">Show keyboard</label>
-        <input type="checkbox" name="" id="calc__checkbox" v-model="checked">
-        <KeyBoard />
+
+        <label for="checkbox">Show keyboard</label>
+        <input type="checkbox" name="checkbox" id="checkbox" v-model="keyboardVisible">
+        
+        <KeyBoard 
+            v-show="keyboardVisible"
+            @changeOperandInput="setOperand"
+            @addDigit="addDigit"
+        />
     </form>
 </template>
 
@@ -22,19 +63,19 @@
 
     export default {
     name: 'CalcComponent',
-
     components: {
-        KeyBoard,
+        KeyBoard
     },
 
     data: () => {
         return {
             result: 0,
-            operand1: 0,
-            operand2: 0,
+            operand1: '',
+            operand2: '',
             base: 0,
             exponent: 0,
-            checked: true,
+            keyboardVisible: true,
+            activeOperandInput: 'operand1'
         }
     },
 
@@ -45,9 +86,43 @@
             else if (event.target.classList.contains('calc__btn-input--diff')) this.result = this.operand1 - this.operand2;
             else if (event.target.classList.contains('calc__btn-input--mult')) this.result = this.operand1 * this.operand2;
             else if (event.target.classList.contains('calc__btn-input--div')) this.result = this.operand1 / this.operand2;    
-            else if (event.target.classList.contains('calc__btn-input--div--int')) this.result = Math.trunc(this.operand1 / this.operand2);    
-            else if (event.target.classList.contains('calc__btn-input--exp')) this.result = Math.pow(this.base, this.exponent);    
+            else if (event.target.classList.contains('calc__btn-input--div--int')) this.result = Math.trunc(this.operand1 / this.operand2);      
+        },
+
+        sum(evt){
+            evt.preventDefault();
+            this.result = Number(this.operand1) + Number(this.operand2);
+        },
+        diff(evt){
+            evt.preventDefault();
+            this.result = Number(this.operand1) - Number(this.operand2);
+        },
+        mult(evt){
+            evt.preventDefault();
+            this.result = Number(this.operand1) * Number(this.operand2);
+        },
+        div(evt){
+            evt.preventDefault();
+            this.result = Number(this.operand1) / Number(this.operand2);
+        },
+        divInt(evt){
+            evt.preventDefault();
+            this.result = Math.trunc(Number(this.operand1) / Number(this.operand2));
+        },
+
+        setOperand(operandInput){
+            this.activeOperandInput = operandInput;
+        },
+
+        addDigit(number){
+            if (this.activeOperandInput === 'operand1'){
+                this.operand1 += number
+            } else if (this.activeOperandInput === 'operand2'){
+                 this.operand2 += number   
+            }    
         }
+
+
     }
 }
 </script>
--- lesson_1/src/components/Keyboard.vue
+++ lesson_1/src/components/Keyboard.vue
@@ -1,18 +1,33 @@
 <template>
     <div class="keyboard">
         <ul class="keyboard__buttons-list">
-            <li class="keyboard__button"><button class="button">0</button></li>
-            <li class="keyboard__button"><button class="button">1</button></li>
-            <li class="keyboard__button"><button class="button">2</button></li>
-            <li class="keyboard__button"><button class="button">3</button></li>
-            <li class="keyboard__button"><button class="button">4</button></li>
-            <li class="keyboard__button"><button class="button">5</button></li>
-            <li class="keyboard__button"><button class="button">6</button></li>
-            <li class="keyboard__button"><button class="button">7</button></li>
-            <li class="keyboard__button"><button class="button">8</button></li>
-            <li class="keyboard__button"><button class="button">9</button></li>
-            <li class="keyboard__button"><button class="button">Delete</button></li>
+            <li class="keyboard__button" v-for="(digit, index) in digits" :key="index">
+                <button 
+                    class="button" 
+                    @click="getNumber($event), addDigit()">
+                            {{digit}}
+                </button>
+            </li>
         </ul>
+
+        <div class="change-input">
+            <label for="change-input__radio1" class="change-input__label">Operand1</label>
+            <input 
+                type="radio" name="change-input__radio" 
+                id="change-input__radio1" 
+                value="operand1"
+                v-model="activeOperandInput"
+                @click="changeOperandInput()" 
+            />
+            <label for="change-input__radio2" class="change-input__label">Operand2</label>
+            <input 
+                type="radio" name="change-input__radio" 
+                id="change-input__radio2" 
+                value="operand2"
+                v-model="activeOperandInput"
+                @click="changeOperandInput()"
+            />
+        </div>
     </div>
 </template>
 
@@ -20,10 +35,33 @@
 
 export default {
     name: 'KeyBoard',
+
+    data: () => {
+        return {
+            digits: [0,1,2,3,4,5,6,7,8,9, 'Delete'],
+            number: '',
+            activeOperandInput: 'operand1'
+        }
+    },
+
+    methods: {
+        getNumber(evt){
+            evt.preventDefault();
+            this.number = evt.target.textContent;
+        },
+
+        changeOperandInput(){
+            this.$emit('changeOperandInput', this.activeOperandInput)
+        },
+
+        addDigit(){
+            this.$emit('addDigit', Number(this.number))
+        }
+    }
 }
 </script>
 
-<style scoped>
+<style>
     .keyboard__buttons-list {
         display: flex;
         list-style: none;
